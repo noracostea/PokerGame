d1f2dbeb5f34d6c593818cdcb04922f4dfa830d7
noracostea <noracostea33@gmail.com>
2025-03-31 19:20:54 +0200

    Started Poker Game

--- game.py
+++ game.py
@@ -1,60 +1,113 @@
 from deck import Deck, Card
 
+
 class Hand:
-    def __init__(self, deck):
-        cards = []
+    """
+    Create the Hand class, which will draw 5 cards and store them
+    """
+
+    def __init__(self):
+        """
+        Draw 5 cards from the deck, using a for loop
+        """
+        hand = []
         for i in range(5):
-            cards.append(deck.deal_card())
-        self._cards = cards
+            hand.append(deck.deal())
+        self._hand = hand
 
     @property
-    def cards(self):
-        return self._cards
+    def hand(self):
+        """
+        Remove the need for (), saving time.
+        returns: the hand
+        """
+        return self._hand
 
     def __str__(self):
-        return str(self.cards)
+        return str(self.hand)
 
     @property
     def is_flush(self):
-        for card in self.cards[1:]:
-            if self.cards[0].suit != card.suit:
+        """
+        Check if the hand is a flush. Established as a property since it is an action.
+        """
+        for card in self.hand:
+            if card.suit != self.hand[0].suit:
                 return False
         return True
 
     @property
-    def is pair(self):
+    def num_matches(self):
         matches = 0
-        for c1 in self.cards:
-            for c2 in self.cards:
-                if c1 == c2:
-                    continue
         for i in range(5):
-            for j in range (5:
+            for j in range(5):
                 if i == j:
-                    continue:
-                if self.cards[i].rank == self.cards[j].rank:
+                    pass
+                elif self.hand[i].rank == self.hand[j].rank:
                     matches += 1
-        if matches == 2:
+        return matches
+
+    @property
+    def is_pair(self):
+        if self.num_matches == 2:
             return True
         return False
 
-matches = 0
-count = 0
-while matches < 10000:
-    deck = Deck()
-    deck.shuffle()
-    hand = Hand(deck)
-    count += 1
-    if hand.is_pair:
-        print(hand)
-        matches += 1
-        #break
+    @property
+    def is_2pair(self):
+        if self.num_matches == 4:
+            return True
+        return False
 
-print(f"The probability of a flush is {100*matches/count}")
+    @property
+    def is_trip(self):
+        if self.num_matches == 6:
+            return True
+        return False
+
+    @property
+    def is_full(self):
+        if self.num_matches == 8:
+            return True
+        return False
+
+    @property
+    def is_quad(self):
+        if self.num_matches == 12:
+            return True
+        return False
 
     @property
     def is_straight(self):
         if self.num_matches != 0:
             return False
-        self.cards.sort()
+        self.hand.sort()
+        if Card.RANKS.index(self.hand[-1].rank) != Card.RANKS.index(self.hand[0].rank) + 4:
+            return False
+        return True
+
+
+# deck = Deck()
+# deck.shuffle()
+# h = Hand()
+# print(h)
+
+# while True:
+#     deck = Deck()
+#     deck.shuffle()
+#     h = Hand()
+#     if h.is_flush:
+#         print(h)
+#         break
+
+count = 0
 matches = 0
+while matches < 1000:
+    deck = Deck()
+    deck.shuffle()
+    h = Hand()
+    count += 1
+    if h.is_straight:
+        matches += 1
+
+print(100 * (matches / count))
